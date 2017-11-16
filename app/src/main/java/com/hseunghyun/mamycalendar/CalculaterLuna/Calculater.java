f634aa7c77f4a5d80715b2f960cd44093d7b038d
Seunghyun.hong <hong.seung.hyun.23@gmail.com>
2017-11-16 15:56:35 +0900

    Date date에서 년월일 추출방법 찾음. c.get(Calendar.DAY_OF_MONTH);

---
+++ app/src/main/java/com/hseunghyun/mamycalendar/CalculaterLuna/Calculater.java
@@ -0,0 +1,18 @@
+package com.hseunghyun.mamycalendar.CalculaterLuna;
+
+import java.util.Date;
+
+/**
+ * Created by Thunder on 2017-11-16.
+ */
+
+public class Calculater {
+    private Date date;
+    private Date lune;
+
+    public Calculater(Date date) {
+        this.date = date;
+    }
+
+
+}
--- app/src/main/java/com/hseunghyun/mamycalendar/MyDate.java
+++ app/src/main/java/com/hseunghyun/mamycalendar/MyDate.java
@@ -1,14 +1,25 @@
 package com.hseunghyun.mamycalendar;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 
 /**
  * Created by Thunder on 2017-11-13.
  */
 
 public class MyDate {
+
+    private SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat("MM.dd", Locale.getDefault());
+
     private Date date;
+    private String datelune;
+    private Date datelun;
     private Date lune;
+    private int dateYear;
+    private int dateMonth;
+    private int dateDayOfMonth;
 
     public MyDate(Date date) {
         this.date = date;
@@ -34,9 +45,51 @@ public class MyDate {
         this.lune = lune;
     }
 
+    public String getDatelune() {
+        return datelune;
+    }
+
+    public void setDatelune(String datelune) {
+        this.datelune = datelune;
+    }
+
+    public Date getDatelun() {
+        return datelun;
+    }
+
+    public void setDatelun(Date datelun) {
+        this.datelun = datelun;
+    }
 
     // 여기서 음력 변환
-    private void convertDateToLune(){
-        lune = date;
+    private void convertDateToLune() {
+
+//        lune =
+
+        Calendar c = Calendar.getInstance();
+        c.setTime(date);
+
+//        date.getTime(); // 이게 롱값이래...
+//        mSimpleDateFormat.format(date.getTime());  // 이건 스트링
+//        datelun.setTime(date.getTime());
+
+        dateYear = c.get(Calendar.YEAR);
+        dateMonth = c.get(Calendar.MONTH);
+        dateDayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+
+
+
+    }
+
+
+    @Override
+    public String toString() {
+        return "MyDate{" +
+                "mSimpleDateFormat=" + mSimpleDateFormat +
+                ", date=" + date +
+                ", datelune='" + datelune + '\'' +
+                ", datelun=" + datelun +
+                ", lune=" + lune +
+                '}';
     }
 }
